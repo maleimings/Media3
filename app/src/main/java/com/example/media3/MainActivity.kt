533963b48486574212fae48c92c50bef942312ea
Randy Ma <maleimings@gmail.com>
2023-07-10 16:09:37 +0800

    add lifecycle

--- app/src/main/java/com/example/media3/MainActivity.kt
+++ app/src/main/java/com/example/media3/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.media3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
@@ -25,7 +33,9 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Media3Theme {
-                VideoView(videoUrl = "https://livesim.dashif.org/livesim/chunkdur_1/ato_7/testpic4_8s/Manifest.mpd")
+                Column {
+                    VideoView(videoUrl = "https://dash.akamaized.net/dash264/TestCases/2c/qualcomm/1/MultiResMPEG2.mpd")
+                }
             }
         }
     }
@@ -36,18 +46,43 @@ fun VideoView(videoUrl: String) {
     val context = LocalContext.current
 
     val player = ExoPlayer.Builder(context).build()
-        .also {
-            exoPlayer ->
+        .also { exoPlayer ->
             val mediaItem = MediaItem.fromUri(videoUrl)
             exoPlayer.setMediaItem(mediaItem)
             exoPlayer.prepare()
+            exoPlayer.play()
         }
 
-    DisposableEffect(AndroidView(factory = {
-        PlayerView(context).apply {
-            this.player = player
+    val lifeCycleOwner = rememberUpdatedState(LocalLifecycleOwner.current)
+
+    DisposableEffect(
+        AndroidView(modifier = Modifier.focusable(), factory = {
+            PlayerView(context).apply {
+                this.player = player
+            }
+        })
+    ) {
+        val observer = LifecycleEventObserver {
+                _, event ->
+            when (event) {
+                Lifecycle.Event.ON_PAUSE -> {
+                    player.pause()
+                }
+
+                Lifecycle.Event.ON_RESUME -> {
+                    player.play()
+                }
+
+                else -> {}
+            }
         }
-    })) {
-        onDispose { player.release() }
+
+        val lifecycle = lifeCycleOwner.value.lifecycle
+        lifecycle.addObserver(observer)
+
+        onDispose {
+            player.release()
+            lifecycle.removeObserver(observer)
+        }
     }
 }
